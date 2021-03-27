2062c887807d014d3bacf26d614876bcdc41f764
HHH917 <917388227@qq.com>
2021-03-27 17:58:32 +0800

    课时61 看了一半

--- components/free-ui/free-popup.vue
+++ components/free-ui/free-popup.vue
@@ -1,125 +1,140 @@
 <template>
-	<div v-if='status' style="z-index:9998;overflow:hidden;">
+	<div v-if="status" style="z-index:9998;overflow:hidden;">
 		<!-- 蒙版 -->
-		<div v-if="mask" @click="hide" class="position-fixed top-0 left-0 right-0 bottom-0 " :style="getMaskColor">
-		</div>
+		<div
+			v-if="mask"
+			@click="hide"
+			class="position-fixed top-0 left-0 right-0 bottom-0 "
+			:style="getMaskColor"
+		></div>
 		<!-- 弹窗框内容 -->
-		<div ref="popup" class="position-fixed  free-animated" :class='getBodyClass' :style='getBodyStyle'>
+		<div
+			ref="popup"
+			class="position-fixed  free-animated"
+			:class="getBodyClass"
+			:style="getBodyStyle"
+		>
 			<slot></slot>
 		</div>
-
 	</div>
 </template>
 
 <script>
-	// #ifdef APP-PLUS-NVUE
-	const animation = weex.requireModule('animation')
-	// #endif
-	export default {
-		props: {
-
-			// 是否开启蒙版颜色
-			maskColor: {
-				type: Boolean,
-				default: false
-			},
-			// 是否开启蒙版
-			mask: {
-				type: Boolean,
-				default: true
-			},
-			// 是否处于底部
-			bottom: {
-				type: Boolean,
-				default: false
-			},
-			// 弹出层内容高度
-			bodyWidth: {
-				type: Number,
-				default: 0,
-			},
-			// 弹出层内容宽度
-			bodyHeight: {
-				type: Number,
-				default: 0
-			},
-			bodyBgColor: {
-				type: String,
-				default: 'bg-white'
-			},
-			transformOrigin: {
-				type: String,
-				default: "left top"
-			},
-			//tabbar高度
-			tabbarHeight:{
-				type:Number,
-				default:0
-			}
+// #ifdef APP-PLUS-NVUE
+const animation = weex.requireModule('animation');
+// #endif
+export default {
+	props: {
+		// 是否开启蒙版颜色
+		maskColor: {
+			type: Boolean,
+			default: false
 		},
-		data() {
-			return {
-				status: false,
-				x: -1,
-				y: -1,
-				maxX: 0,
-				maxY: 0
-			}
+		// 是否开启蒙版
+		mask: {
+			type: Boolean,
+			default: true
 		},
-		mounted() {
-			try {
-				const res = uni.getSystemInfoSync();
-				this.maxX = res.windowWidth - uni.upx2px(this.bodyWidth);
-				this.maxY = res.windowHeight - uni.upx2px(this.bodyHeight) - uni.upx2px(this.tabbarHeight);
-
-				console.log(res.windowWidth);
-				console.log(res.windowHeight);
-			} catch (e) {
-
-			}
+		// 是否处于底部
+		bottom: {
+			type: Boolean,
+			default: false
 		},
-		computed: {
-			getMaskColor() {
-				let i = this.maskColor ? 0.5 : 0;
-				return `background-color: rgba(0,0,0,${i});`
-			},
-			getBodyClass() {
-				let bottom = this.bottom ? 'left-0 right-0 bottom-0' : 'rounded border';
-				return `${this.bodyBgColor} ${bottom}`
-			},
-			getBodyStyle() {
-				let left = this.x > -1 ? `left:${this.x}px;` : '';
-				let top = this.y > -1 ? `top:${this.y}px;` : '';
-				return left + top
-			}
+		// 弹出层内容高度
+		bodyWidth: {
+			type: Number,
+			default: 0
 		},
-		methods: {
-			show(x = -1, y = -1) {
+		// 弹出层内容宽度
+		bodyHeight: {
+			type: Number,
+			default: 0
+		},
+		bodyBgColor: {
+			type: String,
+			default: 'bg-white'
+		},
+		transformOrigin: {
+			type: String,
+			default: 'left top'
+		},
+		//tabbar高度
+		tabbarHeight: {
+			type: Number,
+			default: 0
+		}
+	},
+	data() {
+		return {
+			status: false,
+			x: -1,
+			y: -1,
+			maxX: 0,
+			maxY: 0
+		};
+	},
+	mounted() {
+		try {
+			const res = uni.getSystemInfoSync();
+			this.maxX = res.windowWidth - uni.upx2px(this.bodyWidth);
+			this.maxY =
+				res.windowHeight -
+				uni.upx2px(this.bodyHeight) -
+				uni.upx2px(this.tabbarHeight);
 
-				this.x = x > this.maxX ? this.maxX : x;
-				this.y = y > this.maxY ? this.maxY : y;
-				this.status = true;
-				// #ifdef APP-PLUS-NVUE
-				this.$nextTick(() => {
-					animation.transition(this.$refs.popup, {
+			console.log(res.windowWidth);
+			console.log(res.windowHeight);
+		} catch (e) {}
+	},
+	computed: {
+		getMaskColor() {
+			let i = this.maskColor ? 0.5 : 0;
+			return `background-color: rgba(0,0,0,${i});`;
+		},
+		getBodyClass() {
+			let bottom = this.bottom
+				? 'left-0 right-0 bottom-0'
+				: 'rounded border';
+			return `${this.bodyBgColor} ${bottom}`;
+		},
+		getBodyStyle() {
+			let left = this.x > -1 ? `left:${this.x}px;` : '';
+			let top = this.y > -1 ? `top:${this.y}px;` : '';
+			return left + top;
+		}
+	},
+	methods: {
+		show(x = -1, y = -1) {
+			this.x = x > this.maxX ? this.maxX : x;
+			this.y = y > this.maxY ? this.maxY : y;
+			this.status = true;
+			// #ifdef APP-PLUS-NVUE
+			this.$nextTick(() => {
+				animation.transition(
+					this.$refs.popup,
+					{
 						styles: {
 							transform: 'scale(1,1)',
-							transformOrigin:this.transformOrigin,
+							transformOrigin: this.transformOrigin,
 							opacity: 1
 						},
 						duration: 100, //ms
 						timingFunction: 'ease',
 						delay: 0 //ms
-					}, function() {
-						console.log("动画执行结束");
-					})
-				})
-				// #endif
-
-			},
-			hide() {
-				// #ifdef APP-PLUS-NVUE
-				animation.transition(this.$refs.popup, {
+					},
+					function() {
+						console.log('动画执行结束');
+					}
+				);
+			});
+			// #endif
+		},
+		hide() {
+			this.$emit("hide");
+			// #ifdef APP-PLUS-NVUE
+			animation.transition(
+				this.$refs.popup,
+				{
 					styles: {
 						transform: 'scale(0,0)',
 						transformOrigin: this.transformOrigin,
@@ -128,24 +143,26 @@
 					duration: 100, //ms
 					timingFunction: 'ease',
 					delay: 0 //ms
-				}, () => {
+				},
+				() => {
 					this.status = false;
-					console.log("动画执行结束");
-				})
-				// #endif
-				// #ifndef APP-PLUS-NVUE
-				this.status = false
-				// #endif
-			}
+					console.log('动画执行结束');
+				}
+			);
+			// #endif
+			// #ifndef APP-PLUS-NVUE
+			this.status = false;
+			// #endif
 		}
 	}
+};
 </script>
 
 <style scoped>
-	.free-animated {
-		/* #ifdef APP-PLUS-NVUE */
-		transform: scale(0, 0);
-		opacity: 0;
-		/* #endif */
-	}
+.free-animated {
+	/* #ifdef APP-PLUS-NVUE */
+	transform: scale(0, 0);
+	opacity: 0;
+	/* #endif */
+}
 </style>
